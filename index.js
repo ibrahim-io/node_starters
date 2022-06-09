1e5feb7b0ded38eaff5f8c47c6f6571d9c942c8f
Ibrahim Ibrahim <ii83326@gmail.com>
2022-06-09 12:28:16 +0100

    feat: change port variable to environment variable port or port 3001

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({error: "uknown endpoint"})
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
